59bf79164946fed72509a74d17324529167c3ee3
Andone.Daria <dariaandone03@gmail.com>
2024-06-19 21:11:29 +0300

    other info about linux system and tasks

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
 # practica_an2
 Tool monitorizare sisteme linux prin SSH și centralizare
 
+# Cerinte
+
+1. Pregatesc mediul de comunicare prin ssh dintre cele doua masini virtuale.
+2. Extragere detalii despre performanta generala a sistemului tinta, precum:
+    a. procentul de timp CPU utilizat de procesele din sistem
+    b. cantitatea de memorie RAM utilizata si disponibila
+    c. numarul toatal de procese, procese care ruleaza, procese care asteapta sa fie rulate si load average
+    d. cantitatea de spatiu pe disk utilizat si disponibil pentru partitiile sistemului
+    e. rate de citire si scriere a discurilor
+3. Extragere detalii despre resursele sistemului tinta.
+    a. procese active, utilizare cpu si memorie a fiecarui proces
+    b. cantitatea de tarfic transmisa si primita
+    c. numarul de conexiuni de retea active
+4. Extragere date despre securitate.
+    a. ultimele n intrari din jurnalul de sistem
+    b. conexiunile active din retea
+    c. detalii din jurnalul de autentificare
+    d. sumele de control ale fisierelor din directorul curent
+5. Extragere detalii despre o aplicatie specifica.
+6. EXtragere detalii generale despre sistem (ex. ultimii utilizatori conectati, utilizatorul curent).
+7. Salvarea datelor despre masinile virtuale monitorizate intr-un fisier specific.
+8. Trimiterea rezultatului specific fiecarei masini virtuale pe aceasta, prin ssh.
+
 # 17.06.2024
 README modificat pentru test
-
-modificare noua
+Modificare noua
 
 # 18.06.2024
 
@@ -34,7 +56,51 @@ I. Performanta generala a sistemului
 - am retinut "disk usage folosit" respectiv "disponibil" in variabile, apoi am iterat prin aceste variabile folosind cate un *for* pentru a calcula totalul in ambele cazuri.
 
 5) rate de citire si scriere a discurilor
+- verific daca exista comanda "iostat", iar daca nu, instalez pachetul sysstat folosind comanda: "sudo apt-get install sysstat -y" (-y raspunde automat cu yes la intrebarile legate de instalare)
 - am extras ratele de citire si scriere folosind comanda "iostat" apoi am facut similar cu ce am facut la 4), am iterat prin variabile si am calculat totalul.
 
 
 !! Testarile le-am facut folosind comanda: "ssh daria@10.0.2.15 'bash -s' < monitor.sh" pentru a obtine rezultatele de pe Ubuntu. ("bash -s" este folosit pentru a rula scriptul primit ca parametru la stdin pe masina tinta)
+
+# 19.06.2024
+
+II. Resurse ale sistemului
+
+1) procese active, utilizare cpu si memorie a fiecarui proces
+- extrag informatiile necesare folosind comanda "ps"
+
+2) cantitatea de tarfic transmisa si primita
+- extrag aceste informatii din fisierul "/proc/net/dev"
+
+3) numarul de conexiuni de retea active
+- in acest caz folosesc comanda "ss -s"
+
+III. Securitate
+
+1) ultimele n intrari din jurnalul de sistem
+- folosesc comanda journalctl
+
+2) conexiunile active din retea
+- in acest caz folosesc comanda "netstat -tuln" ( t-> TCP, u -> UDP, l -> listening, n -> adresa ip si porturi)
+
+3) detalii din jurnalul de autentificare
+- extrag datele din "/var/log/auth.log"
+
+4) sumele de control ale fisierelor din directorul curent
+- am doua fisiere: "sumeInitiale", respectiv "sumeVerificare"
+- verific daca fisierul "sumeInitiale" exista, iar in caz contrar il creez calculand sumele de control din directorul curent folosind comanda "sha256sum"
+
+IV. Aplicatii specifice
+
+1) verific anumite detalii despre aplicatia Visual Studio Code
+- am calculat numarul de procese vsc
+- am verificat daca exista pe sistem comanda "top", in cazul in care nu exita o instalez
+- am calculat cat timp cpu consuma aceasta aplicatie
+
+2) verific daca aplicatia este in executie
+- in acest caz folosesc comanda: 'pgrep -x "code"'.
+
+V. Diverse
+1) afisez ultimii utilizatori care au fost conectati pe sistem folosind comanda "last"
+
+2) afisez utilizatorul conectat in momentul respectiv folosind comanda: "whoami"
--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,7 @@
 function procentTimpProcese 
 {
     numar_cpu=$(nproc)
-    cpu_folosit=$(ps -eo %cpu --sort=-%cpu | egrep -v "%CPU")
+    cpu_folosit=$(ps -eo %cpu --sort=-%cpu | grep -vE "%CPU")
     cpu_total=0
 
     for linie in $cpu_folosit
@@ -26,7 +26,7 @@ function procentTimpProcese
 
 function memorieRAM
 {
-    detalii_memorie=$(free -h | egrep -i mem | tr -s ' ')
+    detalii_memorie=$(free -h | grep -iE mem | tr -s ' ')
 
     mem_totala=$(echo "$detalii_memorie" | cut -d' ' -f2)
     mem_folosita=$(echo "$detalii_memorie" | cut -d' ' -f3)
@@ -41,8 +41,8 @@ function detaliiProcese
 {
     load_average=$(uptime | tr -s ' ' | cut -d":" -f5)
     numar_procese=$(ps -e --no-headers | wc -l)
-    numar_procese_r=$(ps -eo stat --no-headers |  egrep "R" | wc -l)
-    numar_procese_w=$(ps -eo stat --no-headers |  egrep "D|I" | wc -l)
+    numar_procese_r=$(ps -eo stat --no-headers |  grep -E "R" | wc -l)
+    numar_procese_w=$(ps -eo stat --no-headers |  grep -E "D|I" | wc -l)
 
 
     echo "Numar_procese: $numar_procese, numar_procese_running: $numar_procese_r, numar_procese_waiting: $numar_procese_w, Load_average: $load_average"
@@ -52,8 +52,8 @@ function detaliiProcese
 
 function spatiuDisk
 {
-    du_folosit=$(df | egrep "/dev/sd" | tr -s ' ' | cut -d ' ' -f3)
-    du_disponibil=$(df | egrep "/dev/sd" | tr -s ' ' | cut -d ' ' -f4)
+    du_folosit=$(df | grep -E "/dev/sd" | tr -s ' ' | cut -d ' ' -f3)
+    du_disponibil=$(df | grep -E "/dev/sd" | tr -s ' ' | cut -d ' ' -f4)
 
     total_folosit=0
     total_disponibil=0
@@ -75,6 +75,15 @@ function spatiuDisk
 
 function rateCitireScriere
 {
+    # pentru "iostat" am nevoie de bibliotecta "sysstat"
+    if ! command -v iostat &> /dev/null
+    then
+        echo "sysstat nu este instalat. Instalare..."
+
+        sudo apt-get update
+        sudo apt-get install sysstat -y
+    fi
+
     rata_citire=$(iostat | tail -n +7 | tr -s ' ' | cut -d' ' -f3 | tr "," ".")
     rata_scriere=$(iostat | tail -n +7 | tr -s ' ' | cut -d' ' -f4 | tr "," ".")
 
@@ -110,3 +119,187 @@ detaliiProcese
 spatiuDisk
 rateCitireScriere
 
+
+
+# RESURSE DE SISTEM
+
+# procese active, utilizare memorie si cpu a fiecarui proces
+
+function infoProcese
+{
+    echo "Utilizarea CPU si memorie a fiecarui proces:"
+    ps -eo pid,%cpu,%mem --sort=-%cpu | head -n 20
+    echo
+}
+
+# cantitatea de trafic trimisa si primita
+
+function traficTransmisPrimit
+{
+    pachete_primite=$(grep -E "enp0s3" /proc/net/dev | tr -s " " | cut -d" " -f3)
+    pachete_trimise=$(grep -E "enp0s3" /proc/net/dev | tr -s " " | cut -d" " -f10)
+
+    echo "Pachete trimise: $pachete_trimise, pachete primite: $pachete_primite"
+    
+}
+
+# numarul de conexiuni de retea active
+
+function conexiuniActive
+{
+    numar_conexiuni=$(ss -s | head -n1 | cut -d" " -f2)
+
+    echo "Numarul de conexiuni active: $numar_conexiuni"
+}
+
+
+echo 
+echo "  Resurse de sistem:"
+echo
+infoProcese
+traficTransmisPrimit
+conexiuniActive
+
+
+# SECURITATE
+
+# activitatea sistemului si mesajele de eroare
+
+function jurnaleSistem
+{
+    jurnal=$(journalctl -n 10)
+    echo "Ultimele 10 intrari in jurnalul de sistem:"
+    echo "$jurnal" 
+}
+
+# conexiunile active din retea
+
+function conexiuniRetea
+{
+    if ! command -v netstat &> /dev/null
+    then
+        echo "netstat nu este instalat. Instalare..."
+
+        sudo apt-get update
+        sudo apt-get install -y net-tools
+    fi 
+
+    conex=$(netstat -tuln)
+    echo 
+    echo "Conexiuni active:"
+    echo "$conex" 
+    echo 
+}
+
+# detalii din jurnalul de autentificare
+
+function jurnalAuth
+{
+    jurnAuth=$(tail -n 10 /var/log/auth.log)
+    echo
+    echo "Ultimele 10 mesaje din jurnalul de autentificare auth.log"
+    echo "$jurnAuth"
+    echo 
+}
+
+# calculez sumele de control a fisierelor din directorul curent
+
+function sumeControl
+{
+    if [ ! -f "sumeInitiale" ]
+    then
+        find . -type f -exec sha256sum {} + > sumeInitiale
+    fi 
+
+    find . -type f -exec sha256sum {} + > sumeVerificare
+
+    
+    if cmp -s "sumeInitiale" "sumeComparare"
+    then
+        echo "Fisierele din sistem NU sunt modificate!"
+    else
+        echo "Fisierele din sistem sunt modificate!"
+    fi
+}
+
+
+echo 
+echo "  Securitate:"
+echo 
+jurnaleSistem
+conexiuniRetea
+sumeControl
+
+
+# APLICATII SPECIFICE -> voi verifica detalii despre aplicatia VisualStudioCode disponibila pe cele doua
+# sisteme linux care participa la testari
+
+# performanta aplicatiei
+
+function performantaApp
+{
+    vsc_procese=$(pgrep -c -x code | wc -l)
+
+    if ! command -v top &> /dev/null
+    then
+        echo "top nu este instalat. Instalare..."
+
+        sudo apt-get update
+        sudo apt-get install procps -y
+    fi 
+
+    folosirecpu=$(top -b -n 1 | grep -E "code$" | tr -s " " | cut -d" " -f10 | tr "," ".")
+
+    if [ -z "$folosirecpu" ]; then
+        echo "Nu există procese code în executie!"
+    else
+        totalcpu=0
+        for folcpu in $folosirecpu
+        do 
+            if [[ "$folcpu" =~ ^[0-9.]+$ ]]
+            then
+                totalcpu=$(echo "$totalcpu + $folcpu" | bc)
+            fi
+        done 
+        echo "Gradul de folosire cpu de catre vsc: $totalcpu%"
+    fi 
+
+    echo "Numarul de procese vsc: $vsc_procese"
+}
+
+# verific daca aplicatia e in executie
+
+function verificaExecutie
+{
+    if pgrep -x "code" > /dev/null
+    then 
+        echo "Visual Studio Code este in executie!"
+    else 
+        echo "Visual Studio Code NU este in executie!"
+    fi 
+}
+
+echo
+echo "  Performanta aplicatiei:"
+echo 
+performantaApp
+verificaExecutie
+
+
+# DIVERSE
+
+function infoExtra
+{
+    ultimiiUtilizatori=$(last | head -n -2 | tr -s " " | cut -d " " -f1| sort | uniq -c | tr -s " " | cut -d" " -f3)
+    echo "Ultimii utilizatori conectati sunt:"
+    echo "$ultimiiUtilizatori"
+
+    utilCurent=$(whoami)
+    echo "Utilizatorul conectat in acest moment: $utilCurent"
+}
+
+
+echo 
+echo "  Informatii extra:"
+echo 
+infoExtra
